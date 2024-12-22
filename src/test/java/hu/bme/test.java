2efac64c32a8a549ac74ed39947a5b7eb61ba001
JabbaTheBlack <mezo.kerekesmark@gmail.com>
2024-12-22 11:21:23 +0100

    Initial commit

---
+++ src/test/java/hu/bme/test.java
@@ -0,0 +1,13 @@
+package hu.bme;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class test {
+  
+    @Test
+    void testAddition() {
+        assertEquals(2, 1 + 1, "1 + 1 should equal 2");
+    }    
+}
