d2cca0881fff1d2376660e876a85890d46fc53c2
Praksz Péter <praksz.peti@gmail.com>
2025-01-15 19:00:20 +0100

    Renameing test file to find the test cases

---
+++ src/test/java/hu/bme/ExampleTest.java
@@ -0,0 +1,13 @@
+package hu.bme;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class ExampleTest {
+  
+    @Test
+    void testAddition() {
+        assertEquals(2, 1 + 1, "1 + 1 should equal 2");
+    }    
+}
