8cdaa53aba83e09a270e5df65edf2e6147b1569d
JabbaTheBlack <mezo.kerekesmark@gmail.com>
2025-03-19 18:38:56 +0100

    Adding Singleton pattern

--- src/main/java/hu/bme/managers/InsectManager.java
+++ src/main/java/hu/bme/managers/InsectManager.java
@@ -7,11 +7,27 @@ import hu.bme.insect.Insect;
 
 public class InsectManager {
     
+    private static volatile InsectManager instance;
     private List<Insect> insects;
     
-    public InsectManager() {
+    private InsectManager() {
         insects = new ArrayList<>();
     }
+
+    public static InsectManager getInstance() {
+        InsectManager result = instance;
+
+        if(result == null) {
+            synchronized(InsectManager.class) {
+                result = instance;
+                if(result == null) {
+                    instance = result = new InsectManager();
+                }
+            }
+        }
+        return result;
+    }
+ 
 
     public void addInsect(Insect insect) {
         insects.add(insect);
