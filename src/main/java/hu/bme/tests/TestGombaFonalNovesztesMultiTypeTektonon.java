dbe699c6958453c4b57c3678997ac3e2360c6ac0
JabbaTheBlack <mezo.kerekesmark@gmail.com>
2025-03-22 21:18:14 +0100

    Refine Test6

--- src/main/java/hu/bme/tests/TestGombaFonalNovesztesMultiTypeTektonon.java
+++ src/main/java/hu/bme/tests/TestGombaFonalNovesztesMultiTypeTektonon.java
@@ -2,6 +2,7 @@ package hu.bme.tests;
 
 import hu.bme.fungi.Hyphae;
 import hu.bme.fungi.Mycelium;
+import hu.bme.fungi.Mycologist;
 import hu.bme.tekton.MultiTypeTekton;
 
 public class TestGombaFonalNovesztesMultiTypeTektonon {
@@ -9,22 +10,29 @@ public class TestGombaFonalNovesztesMultiTypeTektonon {
     public static void test_gombafonal_novesztes_multi_type_tektonon() {
         MultiTypeTekton t1 = new MultiTypeTekton();
         MultiTypeTekton t2 = new MultiTypeTekton();
+        Mycologist mycologist = new Mycologist();
         Mycelium m1 = new Mycelium();
         Hyphae h1 = new Hyphae();
         Hyphae h2 = new Hyphae();
+
+        mycologist.addMycelium(m1);
 
         t1.addNeighbour(t2);
 
         t2.addNeighbour(t2);
         t2.addMycelium(m1);
+        t2.addHyphae(h1);
 
         m1.setCurrentTekton(t2);
+        m1.addHyphae(h1);
 
         h1.addMycelium(m1);
         h1.addHyphae(h2);
+        h1.setCurrentTekton(t2);
 
         h2.addHyphae(h1);
-        // TODO implement the growHyphae function and deicde what to do with the sporeEffect
+
+        System.out.println("[Tester] growHyphaeToTekton(" + h1 + t1 + ") -> [Mycologist]");
+        mycologist.growHyphaeToTekton(h1, t1);
     }
-    
 }
