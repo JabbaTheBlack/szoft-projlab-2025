b2e82de3b0057c14968c163192993e5fa0ce729d
Máté Csanaki <mcsanaki@gmail.com>
2025-03-22 20:52:24 +0100

    Tektons - JavaDoc

--- src/main/java/hu/bme/tekton/Tekton.java
+++ src/main/java/hu/bme/tekton/Tekton.java
@@ -8,23 +8,37 @@ import hu.bme.fungi.Mycelium;
 import hu.bme.fungi.spore.Spore;
 import hu.bme.managers.FungalManager;
 
+/**
+ * Represents a tekton, managing its connections to other tektons and neighbours.
+ */
 public abstract class Tekton {
     
     protected final FungalManager fungalManager;
     private List<Tekton> neighbours;
     private List<Tekton> connectedNeighbours;
 
+    /**
+     * Initializes a new tekton with an empty list of neighbours and connected neighbours.
+     */
     protected Tekton() {
         fungalManager = new FungalManager();
         neighbours = new ArrayList<>();
         connectedNeighbours = new ArrayList<>();
     }
 
+    /**
+     * Breaks the tekton apart, refreshing all connections of the tektons.
+     * Abtract method, implemented in subclasses.
+     */
     public void breakApart() {
         // TODO implement function, add javadoc
         throw new UnsupportedOperationException("Unimplemented method 'breakApart'");
     }
 
+    /**
+     * Connects the tekton to another tekton.
+     * @param tekton The tekton to connect to.
+     */
     public void connectToTekton(Tekton tekton) {
 
         if(!neighbours.contains(tekton)) {
@@ -33,64 +47,121 @@ public abstract class Tekton {
         connectedNeighbours.add(tekton);
     }
 
+    /**
+     * Breaks the connection to another tekton.
+     * @param tekton The tekton to disconnect from.
+     */
     public void breakConnectionTo(Tekton tekton) {
         connectedNeighbours.remove(tekton);
     }
 
+    /**
+     * Refreshes the neighbours of all tektons.
+     */
     public void refreshNeighbours(){
         // TODO implement function, add javadoc
         throw new UnsupportedOperationException("Unimplemented method 'refreshNeighbours'");
     }
 
+    /**
+     * Checks if the tekton is connected to another tekton.
+     * @param tekton The tekton to check connection to.
+     * @return True if the tekton is connected to the other tekton, false otherwise.
+     */
     public boolean isConnectedTo(Tekton tekton) {
         return connectedNeighbours.contains(tekton);
     }
 
+    /**
+     * Adds a neighbour to the list of neighbours.
+     * @param tekton The neighbour to add.
+     */
     public void addNeighbour(Tekton tekton) {
         neighbours.add(tekton);
     }
 
+    /**
+     * Removes a neighbour from the list of neighbours.
+     * @param tekton The neighbour to remove.
+     */
     public void removeNeighbour(Tekton tekton) {
         neighbours.remove(tekton);
     }
 
+    /**
+     * Adds a mycelium to this tekton.
+     * @param mycelium The mycelium to add.
+     */
     public void addMycelium(Mycelium mycelium) {
         if(fungalManager.getMyceliumCount() != 0) {
             fungalManager.addMycelium(mycelium);
         }
     }
 
+    /**
+     * Removes a mycelium from this tekton.
+     * @param mycelium The mycelium to remove.
+     */
     public void removeMycelium(Mycelium mycelium)
     {
         fungalManager.removeMycelium(mycelium);
     }
 
+    /**
+     * Adds a hyphae to this tekton.
+     * @param hyphae The hyphae to add.
+     */
     public void addHyphae(Hyphae hyphae) {
         if(fungalManager.getHyphaeCount() != 0) {
             fungalManager.addHyphae(hyphae);
         }
     }
 
+    /**
+     * Removes a hyphae from this tekton.
+     * @param hyphae The hyphae to remove.
+     */
     public void removeHyphae(Hyphae hyphae) {
         fungalManager.removeHyphae(hyphae);
     }
 
+    /**
+     * Adds a spore to this tekton.
+     * @param spore The spore to add.
+     */
     public void addSpore(Spore spore) {
         fungalManager.addSpore(spore);
     }
 
+    /**
+     * Removes a spore from this tekton.
+     * @param spore The spore to remove.
+     */
     public void removeSpore(Spore spore) {
         fungalManager.removeSpore(spore);
     }
 
+    /**
+     * Returns the list of neighbours.
+     * @return The list of neighbours.
+     */
     public List<Tekton> getNeighbours() {
         return neighbours;
     }
 
+    /**
+     * Returns the list of connected neighbours.
+     * @return The list of connected neighbours.
+     */
     public List<Tekton> getConnectedNeighbours() {
         return connectedNeighbours;
     }
 
+    /**
+     * Checks if this tekton has a specific hyphae.
+     * @param hyphae The hyphae to check for.
+     * @return True if the hyphae is present, false otherwise.
+     */
     public boolean hasHyphae(Hyphae hyphae) {
         return fungalManager.hasHyphae(hyphae);
     }
