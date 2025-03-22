5e572459a02a78da2f902eee0c88a7f06bd301f0
Máté Csanaki <mcsanaki@gmail.com>
2025-03-22 19:10:36 +0100

    Other fungi parts - JavaDoc

--- src/main/java/hu/bme/fungi/Hyphae.java
+++ src/main/java/hu/bme/fungi/Hyphae.java
@@ -5,41 +5,74 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
-
+/**
+ * Represents a hyphae in a fungal network, managing connections to other hyphae and myceliums.
+ */
 public class Hyphae{
     private List<Hyphae> connectedHyphae;
     private List<Mycelium> connectedMyceliums;
 
+    /**
+     * Initializes a new hyphae with empty lists for connected hyphae and myceliums.
+     */
     public Hyphae() {
         connectedHyphae = new ArrayList<>();
         connectedMyceliums = new ArrayList<>();
     }
 
+    /**
+     * Checks if this hyphae is connected to any mycelium.
+     * @return true if connected to at least one mycelium, false otherwise.
+     */
     public boolean isConnectedToMycelium() {
-        // TODO implement function, add javadoc
+        // TODO implement function
         throw new UnsupportedOperationException("Unimplemented method 'isConnectedToMycelium'");
     }
 
+    /**
+     * Adds a mycelium to the list of connected myceliums.
+     * @param mycelium The mycelium to be added.
+     */
     public void addMycelium(Mycelium mycelium) {
         connectedMyceliums.add(mycelium);
     }
 
+    /**
+     * Removes a mycelium from the list of connected myceliums.
+     * @param mycelium The mycelium to be removed.
+     */
     public void removeMycelium(Mycelium mycelium) {
         connectedMyceliums.remove(mycelium);
     }
 
+    /**
+     * Adds a hyphae to the list of connected hyphae.
+     * @param hyphae The hyphae to be added.
+     */
     public void addHyphae(Hyphae hyphae) {
         connectedHyphae.add(hyphae);
     }
 
+    /**
+     * Removes a hyphae from the list of connected hyphae.
+     * @param hyphae The hyphae to be removed.
+     */
     public void removeHyphae(Hyphae hyphae) {
         connectedHyphae.remove(hyphae);
     }
 
+     /**
+     * Returns an unmodifiable list of connected hyphae.
+     * @return List of connected hyphae.
+     */
     public List<Hyphae> getConnectedHyphae() {
         return Collections.unmodifiableList(connectedHyphae);
     }
 
+    /**
+     * Returns an unmodifiable list of connected myceliums.
+     * @return List of connected myceliums.
+     */
     public List<Mycelium> getConnectedMyceliums() {
         return Collections.unmodifiableList(connectedMyceliums);
     }
--- src/main/java/hu/bme/fungi/Mycologist.java
+++ src/main/java/hu/bme/fungi/Mycologist.java
@@ -6,36 +6,67 @@ import java.util.List;
 import hu.bme.fungi.spore.SporeEffect;
 import hu.bme.tekton.Tekton;
 
+/**
+ * Represents a mycologist who manages myceliums and interacts with Tekton.
+ */
 public class Mycologist {
 
     private SporeEffect selectedSporeEffect;
     private List<Mycelium> myceliums;
 
+    /**
+     * Initializes a new mycologist with an empty list of myceliums.
+     */
     public Mycologist() {
         myceliums = new ArrayList<>();
     }
 
+    /**
+     * Chooses a spore effect to be used.
+     * @param sporeEffect the spore effect to be chosen
+     */
     public void chooseSporeEffect(SporeEffect sporeEffect) {
         selectedSporeEffect = sporeEffect;
     }
 
+    /**
+     * Releases spores from a mycelium.
+     * @param mycelium
+     */
     public void releaseSpore(Mycelium mycelium) {   
         mycelium.releaseSpores();
     }
 
+    /**
+     * Upgrades a mycelium.
+     * @param mycelium the mycelium to be upgraded
+     */
     public void upgradeMyceium(Mycelium mycelium) {
         mycelium.upgrade();
     }
 
+    /**
+     * Grows hyphae towards a Tekton.
+     * @param tekton The current Tekton.
+     * @param tekton The target Tekton towards which hyphae are grown.
+     */
     public void growHyphaeToTekton(Tekton tekton) {
-        // TODO implement function, add javadoc
+        // TODO implement function
         throw new UnsupportedOperationException("Unimplemented method 'growHyphaeToTekton' in Mycologist class");
     }
 
+    /**
+     * Adds a mycelium to the list of managed myceliums.
+     * @param mycelium The mycelium to be added.
+     */
     public void addMycelium(Mycelium mycelium) {
         myceliums.add(mycelium);
     }
 
+    /**
+     * Removes a mycelium from the list of managed myceliums, when a mycelium is dies.
+     * @param mycelium The mycelium to be removed.
+     */
     public void removeMycelium(Mycelium mycelium) {
         myceliums.remove(mycelium);    
     }
