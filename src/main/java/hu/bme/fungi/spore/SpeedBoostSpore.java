d702ebc28b4d7ef227718f6897031e9f54d2be23
Máté Csanaki <mcsanaki@gmail.com>
2025-03-22 18:48:59 +0100

    Spores - JavaDoc

--- src/main/java/hu/bme/fungi/spore/SpeedBoostSpore.java
+++ src/main/java/hu/bme/fungi/spore/SpeedBoostSpore.java
@@ -8,13 +8,20 @@ import hu.bme.insect.Insect;
  */
 public class SpeedBoostSpore extends Spore {
     
+    /**
+     * Initializes a SpeedBoostSpore.
+     */
     public SpeedBoostSpore() {
         super();
     }
 
+    /**
+     * Applies the effect of the SpeedBoostSpore on an {@link Insect}.
+     * @param insect the insect to apply the effect on
+     */
     @Override
     public void applyEffect(Insect insect) {
-        // TODO implement the function, add javadoc
+        // TODO implement the function
         throw new UnsupportedOperationException("Unimplemented method 'applyEffect'");
     }
 }
