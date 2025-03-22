9a74d4507f18488e492323fefdf49b315068a229
Praksz Péter <praksz.peti@gmail.com>
2025-03-22 21:06:10 +0100

    implementing removeHyphae in FungalManager and changing to ArrayList

--- src/main/java/hu/bme/managers/FungalManager.java
+++ src/main/java/hu/bme/managers/FungalManager.java
@@ -7,6 +7,9 @@ import hu.bme.interfaces.IHyphaeManager;
 import hu.bme.interfaces.IMyceliumManager;
 import hu.bme.interfaces.ISporeManager;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FungalManager {
     private IMyceliumManager myceliumManager;
     private IHyphaeManager hyphaeManager;
@@ -35,7 +38,21 @@ public class FungalManager {
     }
 
     public void removeHyphae(Hyphae hyphae) {
+
+        System.out.println("[" + this + "] removeHyphae("+ hyphae +") -> [" + hyphaeManager + "]");
+        for (Hyphae rmv : hyphaeManager.getHyphaes()) {
+            System.out.println("[" + hyphaeManager + "] removeHyphae("+ hyphae +") -> [" + rmv + "]");
+            rmv.removeHyphae(hyphae);
+        }
+        //hyphaeManager.getHyphaes().forEach(h -> h.removeHyphae(hyphae));
+        for (Mycelium rmv : myceliumManager.getMyceliums()) {
+            System.out.println("[" + myceliumManager + "] removeHyphae("+ hyphae +") -> [" + rmv + "]");
+            rmv.removeHyphae(hyphae);
+            
+        }
         hyphaeManager.removeHyphae(hyphae);
+
+        //myceliumManager.getMyceliums().forEach(m -> m.removeHyphae(hyphae));
     }
 
     public void removeSpore(Spore spore) {
@@ -52,5 +69,9 @@ public class FungalManager {
 
     public boolean hasHyphae(Hyphae hyphae) {
         return hyphaeManager.hasHyphae(hyphae);
+    }
+
+    public ArrayList<Hyphae> getHyphaes() {
+        return hyphaeManager.getHyphaes();
     }
 }
