2b77592beca454a6b61f4a3514dcb3e2ad2ba1ac
JabbaTheBlack <mezo.kerekesmark@gmail.com>
2025-03-22 21:16:13 +0100

    Adding Hyphae owner and currentTekton

--- src/main/java/hu/bme/fungi/Hyphae.java
+++ src/main/java/hu/bme/fungi/Hyphae.java
@@ -1,18 +1,31 @@
 package hu.bme.fungi;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+
+import hu.bme.tekton.Tekton;
 
 
 public class Hyphae{
     private List<Hyphae> connectedHyphae;
     private List<Mycelium> connectedMyceliums;
+    private Tekton currentTekton;
+    private Mycologist ownner;
 
     public Hyphae() {
         connectedHyphae = new ArrayList<>();
         connectedMyceliums = new ArrayList<>();
+        currentTekton = null;
+        ownner = new Mycologist();
+    }
+
+    public void setOwner(Mycologist owner) {
+        this.ownner = owner;
+    }
+
+    public Mycologist getOwner() {
+        return ownner;
     }
 
     public boolean isConnectedToMycelium() {
@@ -42,5 +55,13 @@ public class Hyphae{
 
     public List<Mycelium> getConnectedMyceliums() {
         return Collections.unmodifiableList(connectedMyceliums);
+    }
+
+    public Tekton getCurrentTekton() {
+        return currentTekton;
+    } 
+
+    public void setCurrentTekton(Tekton currentTekton) {
+        this.currentTekton = currentTekton;
     }
 }
