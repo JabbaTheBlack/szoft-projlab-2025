28403b874c6621c96473fbc620d3c2525bd8087f
JabbaTheBlack <mezo.kerekesmark@gmail.com>
2025-03-22 21:15:37 +0100

    Add Test5

--- src/main/java/hu/bme/tekton/SingleTypeTekton.java
+++ src/main/java/hu/bme/tekton/SingleTypeTekton.java
@@ -1,17 +1,31 @@
 package hu.bme.tekton;
 
 import hu.bme.fungi.Hyphae;
+import hu.bme.fungi.Mycologist;
 
 public class SingleTypeTekton extends Tekton {
     
+    private Mycologist owner;
+
     public SingleTypeTekton() {
         super();
+        owner = null;
     }
 
     @Override
-    public void addHyphae(Hyphae hyphae) {
+    public boolean addHyphae(Hyphae hyphae) {
         // TODO implement function, add javadoc
-        throw new UnsupportedOperationException("Unimplemented method 'addHyphae' in SingelTypeTekton class");
-      
+        if(fungalManager.getHyphaeCount() == 0) {
+            this.owner = hyphae.getOwner();
+            fungalManager.addHyphae(hyphae);
+            return true;
+        } 
+        if (hyphae.getOwner().equals(owner) && owner != null) {
+            fungalManager.addHyphae(hyphae);
+            return true;
+        }
+        
+        System.out.println("False");
+        return false;
     }
 }
