459828177c32da6764a83c205094520411f34af5
Praksz Péter <praksz.peti@gmail.com>
2025-03-23 21:30:21 +0100

    adding initialization to test4 and 7

--- src/main/java/hu/bme/tests/TestGombaFonalNovesztesMultiTypeTektonon.java
+++ src/main/java/hu/bme/tests/TestGombaFonalNovesztesMultiTypeTektonon.java
@@ -21,29 +21,45 @@ public class TestGombaFonalNovesztesMultiTypeTektonon {
     public static void test_gombafonal_novesztes_multi_type_tektonon() {
         MultiTypeTekton t1 = new MultiTypeTekton();
         MultiTypeTekton t2 = new MultiTypeTekton();
-        Mycologist mycologist = new Mycologist();
+        Mycologist mycologis1 = new Mycologist();
+        Mycologist mycologist2 = new Mycologist();
+
         Mycelium m1 = new Mycelium();
+        Mycelium m2 = new Mycelium();
         Hyphae h1 = new Hyphae();
         Hyphae h2 = new Hyphae();
+        Hyphae h3 = new Hyphae();
 
-        mycologist.addMycelium(m1);
+        mycologis1.addMycelium(m1);
+        mycologist2.addMycelium(m2);
 
         t1.addNeighbour(t2);
+        t2.addNeighbour(t1);
 
-        t2.addNeighbour(t2);
-        t2.addMycelium(m1);
-        t2.addHyphae(h1);
+        t2.addMycelium(m2);
+        t2.addHyphae(h2);
+        t2.addHyphae(h3);
 
-        m1.setCurrentTekton(t2);
+        t1.addMycelium(m1);
+        t1.addHyphae(h1);
+        t1.addHyphae(h3);
+
+        m1.setCurrentTekton(t1);
+        m2.setCurrentTekton(t2);
         m1.addHyphae(h1);
+        m2.addHyphae(h2);
 
         h1.addMycelium(m1);
-        h1.addHyphae(h2);
-        h1.addCurrentTekton(t2);
+        h2.addMycelium(m2);
+        
+        h2.addHyphae(h3);
+        h3.addHyphae(h2);
+        h1.addCurrentTekton(t1);
+        h2.addCurrentTekton(t2);
+        h3.addCurrentTekton(t2);
+        h3.addCurrentTekton(t1);
 
-        h2.addHyphae(h1);
-
-        System.out.println("[Tester] growHyphaeToTekton(" + h1 +", "+ t1 + ") -> [Mycologist]");
-        mycologist.growHyphaeToTekton(h1, t1);
+        System.out.println("[Tester] growHyphaeToTekton(" + h3 +", "+ t1 + ") -> [Mycologist]");
+        mycologis1.growHyphaeToTekton(h3, t1);
     }
 }
