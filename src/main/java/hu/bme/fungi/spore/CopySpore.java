8f1639ffd90682f2c6aaca054c390234a4f5046f
Praksz Péter <praksz.peti@gmail.com>
2025-04-04 23:07:30 +0200

    CopySpore constructor added

--- src/main/java/hu/bme/fungi/spore/CopySpore.java
+++ src/main/java/hu/bme/fungi/spore/CopySpore.java
@@ -3,6 +3,10 @@ package hu.bme.fungi.spore;
 import hu.bme.insect.Insect;
 
 public class CopySpore extends Spore{
+    public CopySpore() {
+        super();
+    }
+
     @Override
     public void applyEffect(Insect insect) {
         Insect copy = new Insect(insect);
