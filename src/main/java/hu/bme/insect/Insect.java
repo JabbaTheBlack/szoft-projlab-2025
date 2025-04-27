b23c156ebe000f22c10a827cc79285bf127de5ed
janos03 <janosmikaczo@gmail.com>
2025-04-27 13:41:26 +0200

    kitörlődik a spóra ha megeszi a rovar

--- src/main/java/hu/bme/insect/Insect.java
+++ src/main/java/hu/bme/insect/Insect.java
@@ -5,7 +5,8 @@ import hu.bme.fungi.spore.Spore;
 import hu.bme.tekton.Tekton;
 
 /**
- * Represents an insect that can move between Tektons on hyphae and interact with fungi.
+ * Represents an insect that can move between Tektons on hyphae and interact
+ * with fungi.
  */
 public class Insect {
     private int effectDuration;
@@ -17,6 +18,7 @@ public class Insect {
 
     /**
      * Initializes an insect at a specified Tekton with a given movement speed.
+     * 
      * @param currentTekton The initial Tekton where the insect is located.
      * @param movementSpeed The speed at which the insect moves.
      */
@@ -28,7 +30,8 @@ public class Insect {
     }
 
     /**
-     * Default constructor for an insect. Initializes it at a null Tekton with default movement speed and nutrition.
+     * Default constructor for an insect. Initializes it at a null Tekton with
+     * default movement speed and nutrition.
      */
     public Insect() {
         this(null, 1);
@@ -38,14 +41,16 @@ public class Insect {
 
     /**
      * Associates with an entomologist for population tracking.
+     * 
      * @param owner Entomologist managing this insect
      */
     public void setEntomologist(Entomologist owner) {
         this.owner = owner;
     }
-    
+
     /**
      * Gets managing entomologist.
+     * 
      * @return Associated population controller
      */
     public Entomologist getEntomologist() {
@@ -57,7 +62,7 @@ public class Insect {
      */
     public void tick() {
         effectDuration--;
-        if(effectDuration == 0) {
+        if (effectDuration == 0) {
             movementSpeed = 1;
             canCutHyphae = true;
         }
@@ -65,6 +70,7 @@ public class Insect {
 
     /**
      * Sets the current Tekton where the insect is located.
+     * 
      * @param tekton The new Tekton.
      */
     public void setCurrentTekton(Tekton tekton) {
@@ -73,72 +79,83 @@ public class Insect {
 
     /**
      * Moves the insect to a target Tekton if it is connected to the current Tekton.
+     * 
      * @param targetTekton The target Tekton where the insect wants to move.
      */
     public void move(Tekton targetTekton) {
         // TODO implement function
-        
+
         System.out.println("[Insect] isConenctedTo(" + targetTekton + ") -> [Tekton]");
 
-        if(currentTekton.isConnectedTo(targetTekton)) {
+        if (currentTekton.isConnectedTo(targetTekton)) {
             System.out.println("[Insect] isConnectedTo(" + targetTekton + ") <- [Tekton] {true}");
             System.out.println("[Insect] setCurrentTekton(" + targetTekton + ") -> [Insect]");
             this.setCurrentTekton(targetTekton);
             return;
         }
-        
+
         System.out.println("[Insect] isConnectedTo(" + targetTekton + ") <- [Tekton] {false}");
     }
 
     /**
      * Eats a spore and applies its effect to the insect.
+     * 
      * @param spore The spore that the insect eats.
      */
     public void eatSpore(Spore spore) {
         System.out.println("[Insect] getNutrition() -> [Spore]");
-        nutrition += spore.getNutrition(); 
+        nutrition += spore.getNutrition();
         System.out.println("[Insect] getNutrition() <- [Spore] {" + nutrition + "}");
-        
+
         System.out.println("[Insect] applyEffect(" + this + ") -> [Spore]");
         spore.applyEffect(this);
+        currentTekton.removeSpore(spore);
+
     }
 
     /**
      * Cuts a hyphae and removes it from the connected Tektons and Myceliums.
+     * 
      * @param hyphae The hyphae that the insect cuts.
      */
     public void cutHyphae(Hyphae hyphae) {
         // if(canCutHyphae) {
-        //     System.out.println("[" + this + "] canCutHyphae() -> [" + this + "] ");
-        //     System.out.println("[" + this + "] canCutHyphae() <- [" + this + "] {true}");
-        //     System.out.println("[" + this + "] getConnectedNeighbours() -> [" + currentTekton + "]");
-        //     for(Tekton tekton : currentTekton.getConnectedNeighbours()) {
-        //         if(tekton.hasHyphae(hyphae)) {
-        //             System.out.println("[" + this + "] removeHyphae(" + hyphae + ") -> ["+ tekton +"]");
-        //             tekton.removeHyphae(hyphae);
+        // System.out.println("[" + this + "] canCutHyphae() -> [" + this + "] ");
+        // System.out.println("[" + this + "] canCutHyphae() <- [" + this + "] {true}");
+        // System.out.println("[" + this + "] getConnectedNeighbours() -> [" +
+        // currentTekton + "]");
+        // for(Tekton tekton : currentTekton.getConnectedNeighbours()) {
+        // if(tekton.hasHyphae(hyphae)) {
+        // System.out.println("[" + this + "] removeHyphae(" + hyphae + ") -> ["+ tekton
+        // +"]");
+        // tekton.removeHyphae(hyphae);
 
-        //             System.out.println("[" + tekton + "] breakConnectionTo() -> [" + currentTekton + "]");
-        //             tekton.breakConnectionTo(currentTekton);
-                    
-        //             System.out.println("[" + currentTekton + "] breakConnectionTo() -> [" + tekton + "]");
-        //             currentTekton.breakConnectionTo(tekton);
-        //             break;
-        //         }
-        //     }
-            
-        //     currentTekton.removeHyphae(hyphae);
+        // System.out.println("[" + tekton + "] breakConnectionTo() -> [" +
+        // currentTekton + "]");
+        // tekton.breakConnectionTo(currentTekton);
+
+        // System.out.println("[" + currentTekton + "] breakConnectionTo() -> [" +
+        // tekton + "]");
+        // currentTekton.breakConnectionTo(tekton);
+        // break;
+        // }
+        // }
+
+        // currentTekton.removeHyphae(hyphae);
         // }
         // else {
-        //     System.out.println("[Insect] canCutHyphae() -> [Insect] ");
-        //     System.out.println("[Insect] canCutHyphae() <- [Insect] {false}");
+        // System.out.println("[Insect] canCutHyphae() -> [Insect] ");
+        // System.out.println("[Insect] canCutHyphae() <- [Insect] {false}");
         // }
-        if(canCutHyphae){
+        if (canCutHyphae) {
             hyphae.setTimeToLive(1);
+
         }
     }
 
     /**
      * Returns the current nutrition level of the insect.
+     * 
      * @return The nutrition level.
      */
     public float getNutrition() {
@@ -147,6 +164,7 @@ public class Insect {
 
     /**
      * Sets the movement speed of the insect.
+     * 
      * @param movementSpeed The new movement speed.
      */
     public void setMovementSpeed(float movementSpeed) {
@@ -155,6 +173,7 @@ public class Insect {
 
     /**
      * Sets whether the insect can cut hyphae.
+     * 
      * @param canCutHyphae True if the insect can cut hyphae, false otherwise.
      */
     public void setCanCutHyphae(boolean canCutHyphae) {
@@ -163,14 +182,16 @@ public class Insect {
 
     /**
      * Returns whether the insect can cut hyphae.
+     * 
      * @return True if the insect can cut hyphae, false otherwise.
      */
     public boolean getCanCutHyphae() {
         return canCutHyphae;
-    }   
+    }
 
     /**
      * Returns the current Tekton where the insect is located.
+     * 
      * @return The current Tekton.
      */
     public Tekton getCurrentTekton() {
@@ -179,6 +200,7 @@ public class Insect {
 
     /**
      * Copy constructor for the Insect class.
+     * 
      * @param insect The insect to copy.
      */
     public Insect(Insect insect) {
@@ -190,10 +212,11 @@ public class Insect {
 
     /**
      * Checks if the insect is stunned.
+     * 
      * @return True if the insect is stunned, false otherwise.
      */
     public boolean isStunned() {
-        if(movementSpeed == 0){
+        if (movementSpeed == 0) {
             return true;
         }
         return false;
